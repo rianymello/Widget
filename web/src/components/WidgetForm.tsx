a1ed167141575b5d0e6dbd6b3323683ed71e84e7
rianymello <mello.riany@gmail.com>
2022-05-09 16:30:13 -0300

    onClick

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -27,9 +27,11 @@ const FeedBackTypes = {
     },
 };
 
+type FeedbackType = keyof typeof FeedBackTypes
+
 
 export function WidgetForm() {
-    const [feedBackType, setFeedBackType] = useState(null)
+    const [feedBackType, setFeedBackType] = useState<FeedbackType | null>(null)
 
 
     return (
@@ -50,7 +52,7 @@ export function WidgetForm() {
                     className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col
                      items-center gap-2 border-2 border-transparent
                      hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                   // onClick={() => setFeedBackType(key)}
+                    onClick={() => setFeedBackType(key as FeedbackType)}
                     type="button"
                    > 
 
