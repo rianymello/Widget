e1de3f4ef97eeefc6dda2348e8365047c0c1b3e6
rianymello <mello.riany@gmail.com>
2022-05-11 17:33:46 -0300

    animated screenshot button

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,7 +1,7 @@
-import { ArrowLeft, Camera } from "phosphor-react";
+import { ArrowLeft, } from "phosphor-react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
-
+import { ScreenshotButton } from "../../ScreenshotButton";
 
 interface FeedbackContentStepProps {
     feedbackType: FeedbackType;
@@ -41,13 +41,8 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
 
         <footer className="flex gap-2 mt-2">    
             
-            <button 
-                type="button"
-                className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500" 
-            >
-            <Camera  className="w-6 h-6 text-zinc-100" />
-            </button>
-            
+            <ScreenshotButton />
+
             <button
             type="submit"
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
